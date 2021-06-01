a27dea040cd5fbec7d93cdf06d785e0da41e9ee5
ddatsko <ddatsko010101@gmail.com>
2021-06-01 20:35:21 +0300

    Added routers 4 and 5 to server 1

--- server1/topo.py
+++ server1/topo.py
@@ -18,7 +18,7 @@ class CustomTopology(Topo):
     h1(ping) - s1 - r1
     """
 
-    TUNNELS = {'r_1': [('eth12', 2012), ('eth13', 2013)]}
+    TUNNELS = {'r_1': [('eth13', 2013)], 'r_5': [('eth52', 2052)]}
 
     def build(self):
         """Create custom topo"""
@@ -27,14 +27,25 @@ class CustomTopology(Topo):
         host1Ping = self.addHost("h_1")
         switch1 = self.addSwitch("s_1")
         router1 = self.addHost("r_1", cls=LinuxRouter)
+        router4 = self.addHost("r_4", cls=LinuxRouter)
+        router5 = self.addHost("r_5", cls=LinuxRouter)
 
         # Add links
         self.addLink(host1Ping, switch1,
                      intfName2='s1-eth1',
                      intfName1='h1-eth1')
+
         self.addLink(switch1,
                      router1,
                      intfName1='s1-eth2',
                      intfName2='r1-eth1')
+
+        self.addLink(router1, router4,
+                     intfName1='r1-eth2',
+                     intfName2='r4-eth1')
+        self.addLink(router4, router5,
+                     intfName1='r4-eth2',
+                     intfName2='r5-eth1')
+
 
 
